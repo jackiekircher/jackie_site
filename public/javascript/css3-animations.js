728677dcd5c5ba73c8cb0ee85d4b64b49d00f976
jackie kircher <kircher.jackie@gmail.com>
2014-09-05 16:45:41 -0400

    merge in update to css3_animations for captions
    side bonus: also load them into the animations page. this ends up being
    kind of weird with a div.caption containing a div#caption but ... eh

--- public/javascript/css3-animations.js
+++ public/javascript/css3-animations.js
@@ -1,8 +1,8 @@
 $('a.load-animation').click(function() {
-  url = $(this).attr('href');
   css = url.slice(0,-5) + ".css";
   $('style[name="animation"]').load(css);
   $('.animation-container').load(url + " #animation");
+  $('.caption').load(url + " #caption");
 
   return false;
 });
